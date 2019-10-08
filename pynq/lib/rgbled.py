a5c01d1cc35b8d5f7740c13fd02398b093818e6b
Giuseppe Natale <gnatale@xilinx.com>
2019-10-08 16:45:35 +0000

    Modify rgbled.py to allow for arbitrary indexes, and enable use of offset for mmio read/writes.

--- pynq/lib/rgbled.py
+++ pynq/lib/rgbled.py
@@ -37,7 +37,6 @@ __email__ = "pynq_support@xilinx.com"
 
 
 RGBLEDS_XGPIO_OFFSET = 0
-RGBLEDS_START_INDEX = 4
 RGB_CLEAR = 0
 RGB_BLUE = 1
 RGB_GREEN = 2
@@ -54,32 +53,50 @@ class RGBLED(object):
     Attributes
     ----------
     index : int
-        The index of the RGB LED, from 4 (LD4) to 5 (LD5).
+        The index of the RGB LED. Can be an arbitrary value.
+    offset : int
+        When greater than 0, it will be used as offset to access the specific
+        RGBLED, instead of relying on the global value `_rgbleds_start_index`.
     _mmio : MMIO
         Shared memory map for the RGBLED GPIO controller.
     _rgbleds_val : int
         Global value of the RGBLED GPIO pins.
-        
+    _rgbleds_start_index : int
+        Global value representing the lowest index for RGB LEDs
     """
     _mmio = None
     _rgbleds_val = 0
+    _rgbleds_start_index = float("inf")
 
-    def __init__(self, index):
+    def __init__(self, index, ip_name="rgbleds_gpio",
+                 offset=0):
         """Create a new RGB LED object.
         
         Parameters
         ----------
         index : int
-            Index of the RGBLED, from 4 (LD4) to 5 (LD5).
-        
+            Index of the RGBLED, Can be an arbitrary value.
+            The smallest index given will set the global value
+            `_rgbleds_start_index`, unless a valid `offset` is given.
+        ìp_name : str
+            Name of the IP in  the `ip_dict`. Defaults to "rgbleds_gpio".
+        offset : int
+            If defined, will be used as offset to access the specific RGBLED,
+            instead of relying on the global value `_rgbleds_start_index`.
         """
-        if index not in [4, 5]:
-            raise ValueError("Index for onboard RGBLEDs should be 4 or 5.")
-            
+
         self.index = index
         if RGBLED._mmio is None:
-            base_addr = PL.ip_dict["rgbleds_gpio"]["phys_addr"]
+            base_addr = PL.ip_dict[ip_name]["phys_addr"]
             RGBLED._mmio = MMIO(base_addr, 16)
+        if offset == 0 and index < RGBLED._rgbleds_start_index:
+            RGBLED._rgbleds_start_index = index
+        if offset < 0:
+            raise ValueError("Offset cannot be a negative value.")
+        if offset % 4 != 0:
+            raise ValueError("Offset must be a multiple of 4.")
+        self._offset = offset
+        self._val = 0
 
     def on(self, color):
         """Turn on a single RGB LED with a color value (see color constants).
@@ -97,10 +114,14 @@ class RGBLED(object):
         if color not in range(8):
             raise ValueError("color should be an integer value from 0 to 7.")
 
-        rgb_mask = 0x7 << ((self.index-RGBLEDS_START_INDEX)*3)
-        new_val = (RGBLED._rgbleds_val & ~rgb_mask) | \
-                  (color << ((self.index-RGBLEDS_START_INDEX)*3))
-        self._set_rgbleds_value(new_val)
+        if self._offset > 0:
+            self._val = self._val & ~0x7 | color
+            self._set_rgbleds_value(self._val, self._offset)
+        else:
+            rgb_mask = 0x7 << ((self.index-RGBLED._rgbleds_start_index)*3)
+            new_val = (RGBLED._rgbleds_val & ~rgb_mask) | \
+                      (color << ((self.index-RGBLED._rgbleds_start_index)*3))
+            self._set_rgbleds_value(new_val)
 
     def off(self):
         """Turn off a single RGBLED.
@@ -110,9 +131,13 @@ class RGBLED(object):
         None
         
         """
-        rgb_mask = 0x7 << ((self.index-RGBLEDS_START_INDEX)*3)
-        new_val = RGBLED._rgbleds_val & ~rgb_mask
-        self._set_rgbleds_value(new_val)
+        if self._offset > 0:
+            self._val = self._val & ~0x7
+            self._set_rgbleds_value(self._val, self._offset)
+        else:
+            rgb_mask = 0x7 << ((self.index-RGBLED._rgbleds_start_index)*3)
+            new_val = RGBLED._rgbleds_val & ~rgb_mask
+            self._set_rgbleds_value(new_val)
         
     def write(self, color):
         """Set the RGBLED state according to the input value.
@@ -138,11 +163,13 @@ class RGBLED(object):
             The color value stored in the RGBLED.
             
         """
-        return (RGBLED._rgbleds_val >> 
-                ((self.index-RGBLEDS_START_INDEX)*3)) & 0x7
+        if self._offset > 0:
+            return self._val
+        return (RGBLED._rgbleds_val >>
+                ((self.index-RGBLED._rgbleds_start_index)*3)) & 0x7
 
     @staticmethod
-    def _set_rgbleds_value(value):
+    def _set_rgbleds_value(value, offset=0):
         """Set the state of all RGBLEDs.
         
         Note
@@ -154,7 +181,18 @@ class RGBLED(object):
         ----------
         value : int 
             The value of all the RGBLEDs encoded in a single variable.
-        
+            If `offset` is specified, it only represents the value of the 
+            single RGBLED.
+        offset : int
+            If specified, it will be used as offset to access the RGBLED shared
+            memory map. This will prevent the global value `_rgbleds_val` from
+            being updated.
+
         """
-        RGBLED._rgbleds_val = value
-        RGBLED._mmio.write(RGBLEDS_XGPIO_OFFSET, value)
+        if offset < 0:
+            raise ValueError("Offset cannot be a negative value.")
+        if offset % 4 != 0:
+            raise ValueError("Offset must be a multiple of 4.")
+        if offset == 0:
+            RGBLED._rgbleds_val = value
+        RGBLED._mmio.write(RGBLEDS_XGPIO_OFFSET + offset, value)
